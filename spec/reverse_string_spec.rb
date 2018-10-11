9567e15c79be07604a7b2f7a228b8b1171e4dacc
Nick-Damico <nickalan82@icloud.com>
2018-10-11 11:32:45 -0400

    reverse_string test expectations completed in spec/reverse_string_spec.rb

--- spec/reverse_string_spec.rb
+++ spec/reverse_string_spec.rb
@@ -2,9 +2,12 @@ require 'spec_helper'
 
 RSpec.describe "reverse_string" do
   describe "#reverse_string" do
-    it "it outputs hello world" do
-      output = reverse_string
-      expect( output ).to eq("Hello World")
+    it "it returns a reverse of a string argument" do
+      expect(reverse_string("abc")).to eq("cba")
+    end
+
+    it "it reverses a string with spaces" do
+      expect(reverse_string("hello  ")).to eq("  olleh")
     end
   end
 end
