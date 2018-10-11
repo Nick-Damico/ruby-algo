9973d816c998de4beca99fce5527976f98e796d2
Nick-Damico <nickalan82@icloud.com>
2018-10-11 12:09:54 -0400

    updated directions in reverse_string.rb

--- reverse_string/reverse_string.rb
+++ reverse_string/reverse_string.rb
@@ -2,34 +2,10 @@ require 'pry'
 # -- Directions
 #   Run Tests using command 'rspec spec/reverse_string_spec.rb'
 #   Try Solving problem without using the String method '.reverse'
-#   Test Solutions by uncommenting code blocks.
 
 # -- Example
 #   reverse_string('hello') == 'olleh'
 #   reverse_string('  bye') == 'eyb  '
 
-# -- Solution One
-# def reverse_string(str)
-#   reversed = ''
-#   for i in str.split('')
-#     reversed = i + reversed
-#   end
-#   reversed
-# end
-
-# -- Solution Two
-# def reverse_string(str)
-#   reversed = ''
-#   i = 0
-#   while i < str.length
-#     reversed = str[i] + reversed
-#     i += 1
-#   end
-#   reversed
-# end
-
-
-# -- Solution Three
-# def reverse_string(str)
-#   str.reverse
-# end
+def reverse_string(str)
+end
