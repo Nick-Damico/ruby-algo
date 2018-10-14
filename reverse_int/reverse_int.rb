6210e5fca7e60b2af192e4d1a93ad585cf81562e
Nick-Damico <nickalan82@icloud.com>
2018-10-13 22:34:41 -0400

    solution Two added to reverse_int.rb

--- reverse_int/reverse_int.rb
+++ reverse_int/reverse_int.rb
@@ -9,9 +9,16 @@ require 'pry'
 #   reverseInt(-15) === -51
 #   reverseInt(-90) === -9
 
-def reverse_int(int)
-  return int if int === 0
-  is_negative = int < 0 ? true : false
-  reversed_int = int.to_s.reverse.to_i
-  is_negative ? -(reversed_int) : reversed_int
-end
+
+# -- Solution One
+# def reverse_int(int)
+#   return int if int === 0
+#   is_negative = int < 0 ? true : false
+#   reversed_int = int.to_s.reverse.to_i
+#   is_negative ? -(reversed_int) : reversed_int
+# end
+
+# -- Solution Two
+# def reverse_int(int)
+#   ("++-"[int <=> 0] + int.to_s.reverse).to_i
+# end
