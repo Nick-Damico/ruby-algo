f8b90928a564fcbcf86085fcc227f30fe552e3a6
Nick-Damico <nickalan82@icloud.com>
2018-10-14 10:43:28 -0400

    added fizzbuzz.rb, fizzbuzz_spec.rb. fizzbuzz.rb require statement added to spec_helper.rb

---
+++ fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,9 @@
+
+
+require 'pry'
+
+def fizzbuzz(num)
+  5.times do
+    puts 'hello'
+  end
+end
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+RSpec.describe "fizzbuzz" do
+  describe '#fizzbuzz' do
+    it "Calling fizzbuzz with '5' prints out 5 statements" do
+    end
+  end
+end
