93198b7aadc7b69e0463b425e11cdadd945040a7
Nick-Damico <nickalan82@icloud.com>
2018-12-03 10:06:15 -0500

    Add solution 3 with while..loop

--- capitalize/capitalize.rb
+++ capitalize/capitalize.rb
@@ -10,11 +10,33 @@ require 'pry'
 #   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 # Solution 1
-def capitalize(string)
-  # Loop through each word in the sentence
-  capitalized_array = string.split(' ').map do |word|
-    word[0].capitalize + word.slice(1, word.size)
-  end
-  # Join array into a white space separated string
-  capitalized_array.join(' ')
-end
+# def capitalize(string)
+#   # Loop through each word in the sentence
+#   capitalized_array = string.split(' ').map do |word|
+#     word[0].capitalize + word.slice(1, word.size)
+#   end
+#   # Join array into a white space separated string
+#   capitalized_array.join(' ')
+# end
+
+# Solution 2 More concise example of Solution 1
+# def capitalize(string)
+#   string.split(' ').map { |w| w.capitalize }.join(' ')
+# end
+
+# Solution 3
+# def capitalize(string)
+#   result = string[0].capitalize
+#   # Counter
+#   i = 1
+#
+#   while i < string.length
+#     if string[i - 1] == ' '
+#       result += string[i].capitalize
+#     else
+#       result += string[i]
+#     end
+#     i += 1
+#   end
+#   result
+# end
