208619ebe7b44e8345d796495096229be0a7f660
Nick-Damico <nickalan82@icloud.com>
2018-12-03 09:39:17 -0500

    Fix error in capitalize.spec

--- spec/capitalize_spec.rb
+++ spec/capitalize_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 RSpec.describe '#capitalize' do
   it 'should capitalize the first letter of every word in a sentence' do
-    expect(capitalize('hi there, i hope you are doing well.')).to eq('Hi There, I Hope You Are Doing Well')
+    expect(capitalize('hi there, i hope you are doing well.')).to eq('Hi There, I Hope You Are Doing Well.')
   end
 end
